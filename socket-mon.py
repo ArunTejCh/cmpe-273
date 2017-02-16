93bc7103d61f643c69f9155af260129ff1526d1a
Arun Tej <aruntej@outlook.com>
2017-02-15 17:34:09 -0800

    CMPE 273 Lab 1 socket-mon.py

--- socket-mon.py
+++ socket-mon.py
@@ -1,5 +1,27 @@
 import psutil
+import operator
 
+dic = {}
+conns = psutil.net_connections(kind='tcp')
+for proc in conns:
+    if len(proc.laddr) > 0 and len(proc.raddr) > 0:
+        if not proc.pid in dic:
+            dic[proc.pid] = 1
+        else:
+            dic[proc.pid] += 1
+#print dic
+noOfConns =[]
+for key, val in dic.items():
+    noOfConns.append((key,val))
 
-for proc in psutil.net_connections(kind='inet4'):
-    print(proc)
+noOfConns.sort(key=operator.itemgetter(1), reverse=True)
+
+#print noOfConns
+
+print '"pid",  "laddr",  "raddr",  "status"'
+
+for i,e in enumerate(noOfConns):
+    for proc in conns:
+        if e[0] == proc.pid and len(proc.laddr) > 0 and len(proc.raddr) > 0:
+            print '"'+str(proc.pid)+ '", "' + proc.laddr[0]+ '", "' + proc.raddr[0]+ '", "' + proc.status +'"'
+
